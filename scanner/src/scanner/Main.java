0655e78be78b8d060dcfce56a1c7e800fb060a3a
GeoffroyAlarcon <geoffroy.alarcon@gmail.com>
2020-11-25 15:27:25 +0100

    exemple d'utilisation scanner

---
+++ scanner/src/scanner/Main.java
@@ -0,0 +1,15 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Veuillez saisir un premier entier : ");
+		int a = scanner.nextInt();
+
+	}
+
+}
